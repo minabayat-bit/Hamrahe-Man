934033eede7542141a44ac457aef59542e40ec63
minabayat <minabayatt9@gmail.com>
2025-12-07 16:40:00 +0330

    v5

---
+++ src/pages/Invoice.tsx
@@ -0,0 +1,27 @@
+const Invoice = () => {
+  return (
+    <div className="bg-gray-200">
+      <div>
+        <div className="bg-white">
+          <h2>ljkanxlckas</h2>
+          <svg
+            fill="none"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            height="100%"
+            width="100%"
+            preserveAspectRatio="xMidYMid meet"
+            focusable="false"
+          >
+            <defs></defs>
+            <circle cx="12" cy="20" r="2" fill="#0095da"></circle>
+            <circle cx="12" cy="12" r="2" fill="#ff4f00"></circle>
+            <circle cx="12" cy="4" r="2" fill="#0095da"></circle>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Invoice;
